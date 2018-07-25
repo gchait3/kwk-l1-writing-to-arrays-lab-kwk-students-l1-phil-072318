05989cf92cd966f56cf4d8f62784fe96c8f852cf
Grace T <gtsai64@gmail.com>
2018-07-25 15:16:52 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -8,6 +8,7 @@ def change_rainbow_colors
   # Write a solution that corrects the three colors in rainbow_colors, then returns the corrected array
   RAINBOW_COLORS = [0]"red", [1]"light_red", [2]"light_yellow"
 end
+change_rainbow_colors
 
 def add_colors
   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
